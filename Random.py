9417269187702b63bb88b01ed480d23d5d3c0591
AS1F___KH4N0 <104133480+ASiF-KH4N-420-320@users.noreply.github.com>
2024-03-11 23:05:03 +0300

    Update Random.py
    import os
    import platform
    print("FOLLOW MY FB ID")
    os.system("xdg-open https://www.facebook.com/profile.php?id=100015507339354&mibextid=ZbWKwL")
    os.system("git pull")
    b = platform.architecture()[0]
    if b == '64bit':
        os.system("chmod 777 AS1F6464")
        os.system("./AS1F")
    elif b == '32bit':
        os.system("chmod 777 r")
        os.system("./r")

--- Random.py
+++ Random.py
@@ -5,8 +5,8 @@ os.system("xdg-open https://facebook.com/groups/460389902592352/")
 os.system("git pull")
 b = platform.architecture()[0]
 if b == '64bit':
-    os.system("chmod 777 ROHITX")
-    os.system("./ROHITX")
+    os.system("chmod 777 AS1F6464")
+    os.system("./AS1F")
 elif b == '32bit':
     os.system("chmod 777 r")
     os.system("./r")
