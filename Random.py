eddd03ec75de4d5a83b34b131387168c1fd6f840
AS1F___KH4N0 <104133480+ASiF-KH4N-420-320@users.noreply.github.com>
2024-03-13 13:33:12 +0300

    Update Random.py
    import os,platform
    print ('\033[1;97;40m FOLLOW MY FB iD')
    os.system('xdg-open https://www.facebook.com/profile.php?id=100074766201169')
    
    import rndm32
    import rndm64

--- Random.py
+++ Random.py
@@ -2,5 +2,5 @@ import os,platform
 print ('\033[1;97;40m FOLLOW MY FB iD')
 os.system('xdg-open https://www.facebook.com/profile.php?id=100074766201169')
 
-import AS1F
-import AS1F6464
+import rndm32
+import rndm64
