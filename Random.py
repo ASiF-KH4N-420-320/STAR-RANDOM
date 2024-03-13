ff1a1f232524fbd638a3f3a32d9a8cf0596eebd5
AS1F___KH4N0 <104133480+ASiF-KH4N-420-320@users.noreply.github.com>
2024-03-13 13:58:04 +0300

    Update Random.py

--- Random.py
+++ Random.py
@@ -6,7 +6,7 @@ from platform import uname
 arch=uname().machine.lower()
 if 'rrr' in rrr:
     print(' \n\033[1;37m Congratulations! Your Device Support This Tools');time.sleep(2)
-    import 64
+    import fb4
 else:
     print(' \033[1;31m Sorry system or device not support this tools');exit()
   
